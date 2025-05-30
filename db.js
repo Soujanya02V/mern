0a40f4f9236dd4954e24cf611cbeb5d2caf65860
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-05-30 22:35:56 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 //this is the backend file connecting to mongo db
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://soujanyabailawad:*********@cluster0.7wabyf2.mongodb.net/gofood?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = 'database_url';
 
 const mongoDB = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log("Connected to database");
 
-    const fetched_data = mongoose.connection.db.collection("gofooddata");
+    const fetched_data = mongoose.connection.db.collection("collection_name");
     const data = await fetched_data.find({}).toArray();
 
     console.log("Data fetched from gofooddata:", data);
