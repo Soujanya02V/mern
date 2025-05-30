4d0471e0bbdc328733fbdc0e9963a386a79f5d1c
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-05-30 22:25:01 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb+srv://soujanyabailawad:Soujanya@cluster0.7wabyf2.mongodb.net/gofood?retryWrites=true&w=majority&appName=Cluster0';
+
+const mongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to database");
+
+    const fetched_data = mongoose.connection.db.collection("gofooddata");
+    const data = await fetched_data.find({}).toArray();
+
+    console.log("Data fetched from gofooddata:", data);
+  } catch (err) {
+    console.error("Database connection error:", err);
+  }
+};
+
+module.exports = mongoDB;
+
+
+
