5fce5c345e9220b369cf550d6dda5d10898e29d0
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-05-31 22:21:20 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,27 @@
+const mongoose = require('module');
+const {Schema} = mongoose;
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        requireed: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    Date:{
+        type: Date,
+        default:Date.now
+    }
+});
+
+module.exports = mongoose.model('user', UserSchema)
