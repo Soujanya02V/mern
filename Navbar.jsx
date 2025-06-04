601a4c5ea2b7f5bb516ff0ce0a698deb25e044dc
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-06-04 13:52:58 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link , useNavigate} from 'react-router-dom';
-
+import { Badge } from 'react-bootstrap';
 
 export default function Navbar() {
+
+  const [cartView, setCartView] = useState(false)
 
 const navigate = useNavigate();
 
@@ -47,9 +49,14 @@ const handleLogout = () =>{
         :
         <div className='d-flex'>
         <div>
-            <div className='btn bg-white text-success mx-2'>
-          MyCart
+            <div className='btn bg-white text-success mx-2' onClick={ () =>{
+              setCartView(true)
+            }}>
+          MyCart { " "}
+          <Badge pill bg="danger">2</Badge>
         </div>
+
+        {cartView? <Modal onClose={ ()=>setCartView(false)}></Modal> : null}
         </div>
         <div className='btn bg-white text-danger  mx-2' onClick={handleLogout}>
           Logout
@@ -62,4 +69,5 @@ const handleLogout = () =>{
    </div>
   )
 }
+
 
