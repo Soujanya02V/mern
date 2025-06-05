321e9db5757edf24d1f4b1ab6240f0ccc9141506
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-06-05 12:05:33 +0530

    Update Cart.js

--- Cart.js
+++ Cart.js
@@ -1,80 +1,63 @@
-import React from 'react'
-import Delete from '@material-ui/icons/Delete'
+import React from 'react';
 import { useCart, useDispatchCart } from '../components/ContextReducer';
-export default function Cart() {
-  let data = useCart();
+
+export default function Cart({onClose}) {
+  let  data = useCart();
   let dispatch = useDispatchCart();
-  if (data.length === 0) {
+
+  console.log("Cart component data:", data);
+
+  if (!data || data.length === 0) {
     return (
       <div>
-        <div className='m-5 w-100 text-center fs-3'>The Cart is Empty!</div>
+        <div className="m-5 w-100 text-center fs-3">The Cart is Empty!!</div>
+        
       </div>
-    )
-  }
-  // const handleRemove = (index)=>{
-  //   console.log(index)
-  //   dispatch({type:"REMOVE",index:index})
-  // }
-
-  const handleCheckOut = async () => {
-    let userEmail = localStorage.getItem("userEmail");
-    // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("http://localhost:5000/api/auth/orderData", {
-      // credentials: 'include',
-      // Origin:"http://localhost:3000/login",
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        order_data: data,
-        email: userEmail,
-        order_date: new Date().toDateString()
-      })
-    });
-    console.log("JSON RESPONSE:::::", response.status)
-    if (response.status === 200) {
-      dispatch({ type: "DROP" })
-    }
+    );
   }
 
-  let totalPrice = data.reduce((total, food) => total + food.price, 0)
+  let  totalPrice = data.reduce((total, food) => total + food.price, 0);
+
   return (
     <div>
-
-      {console.log(data)}
-      <div className='container m-auto mt-5 table-responsive  table-responsive-sm table-responsive-md' >
-        <table className='table table-hover '>
-          <thead className=' text-success fs-4'>
+      <div className="container m-auto mt-5 table-responsive table-responsive-sm table-responsive-md">
+        <table className="table table-hover">
+          <thead className="text-success fs-4">
             <tr>
-              <th scope='col' >#</th>
-              <th scope='col' >Name</th>
-              <th scope='col' >Quantity</th>
-              <th scope='col' >Option</th>
-              <th scope='col' >Amount</th>
-              <th scope='col' ></th>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Option</th>
+              <th scope="col">Amount</th>
+              <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
             {data.map((food, index) => (
-              <tr>
-                <th scope='row' >{index + 1}</th>
-                <td >{food.name}</td>
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{food.name}</td>
                 <td>{food.qty}</td>
                 <td>{food.size}</td>
-                <td>{food.price}</td>
-                <td ><button type="button" className="btn p-0"><Delete onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button> </td></tr>
+                <td>₹{food.price}/-</td>
+
+                <td>
+                  <button
+                    type="button"
+                    className="btn btn-danger btn-sm"
+                    onClick={() => dispatch({ type: "REMOVE", index:index })}
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
             ))}
           </tbody>
         </table>
-        <div><h1 className='fs-2'>Total Price: {totalPrice}/-</h1></div>
-        <div>
-          <button className='btn bg-success mt-5 ' onClick={handleCheckOut} > Check Out </button>
-        </div>
+        <div className="fs-5 fw-bold text-success">Total: ₹{totalPrice}/-</div>
+        <button className="btn bg-success mt-3">Check Out</button>
+        {onClose && <button className="btn btn-danger mt-3 ms-3" onClick={onClose}>Close</button>}
       </div>
-
-
-
     </div>
-  )
+  );
 }
